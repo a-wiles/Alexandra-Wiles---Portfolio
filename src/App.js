3f04abc4ee6f5810e412ef2aa420568854b92944
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-06-26 18:40:13 -0400

    Fixed file structure again

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css';
-import About from './components/About';
-import Contact from './components/Contact';
 import Header from './components/Header';
-import Nav from './components/Nav';
-import Projects from './components/Projects';
-import Resume from './components/Resume';
 
 function App() {
-    const [currentPage, setCurrentPage] = useState('About');
-
-    const renderPage = () => {
-      if (currentPage === 'About') {
-        return <About />;
-      }
-      if (currentPage === 'Projects') {
-        return <Projects />;
-      }
-      if (currentPage === 'Resume') {
-        return <Resume />;
-      }
-      return <Contact />;
-    };
-
-    const handlePageChange = (page) => setCurrentPage(page);
-
 
     return (
       <div>
        <div>
-      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
-      {renderPage()}
+        <Header />
     </div>
       </div>
     );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../App.css';
+import About from '../About';
+import Contact from '../Contact';
 import Nav from '../Nav';
+import Projects from '../Projects';
+import Resume from '../Resume';
 
-export default function Header() {
-  return(
-    <div>
-      
+function App() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+      if (currentPage === 'About') {
+        return <About />;
+      }
+      if (currentPage === 'Projects') {
+        return <Projects />;
+      }
+      if (currentPage === 'Resume') {
+        return <Resume />;
+      }
+      return <Contact />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+
+    return (
+      <div>
+       <div>
+        <h1>Alexandra Wiles</h1>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
     </div>
-  );
-}
+      </div>
+    );
+  }
+
+
+export default App;
