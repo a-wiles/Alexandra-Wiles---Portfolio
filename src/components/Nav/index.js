94b0182ee0b7d9ecfdba77c9ea97cf249589844a
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-06-26 14:42:40 -0400

    Linking header and nav based upon previous portfolio

---
+++ src/components/Nav/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Nav () {
+    return(
+        <header>
+        <nav>
+          <ul>
+    
+            <li>
+              <a href="#aboutme">About Me</a>
+            </li>
+
+            <li>
+              <a href="#portfolio">Portfolio</a>
+            </li>
+
+            <li>
+              <a href="#contactme">Contact Me</a>
+            </li>
+    
+          </ul>
+        </nav>
+      </header>
+    )
+}
