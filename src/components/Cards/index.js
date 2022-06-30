d6b44cf92b7f64c4c107c57b55aafb5cae1299cd
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-06-30 02:11:50 -0400

    Added second project

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -6,6 +6,7 @@ import genie from '../../assets/moviegenie.jpg';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import blogsite from '../../assets/blogsite.png';
 
 
 export default function Cards() {
@@ -18,19 +19,37 @@ export default function Cards() {
                             <Card.Title>Movie Genie</Card.Title>
                             <img src={genie} width="100%" />
                             <Card.Text>
-                                Have you ever heard about a movie, were debating if you wanted to see it, but wanted to see more than just a rating from your typical critic? Well, we have just the application for you! Rub the lamp, and have your wish fulfilled! The Movie Genie is an application made with a lot of determination, that will alow you to search for any movie you want and see quick reviews, a user-rating and plot-outline. Super useful, right? Just type out whatever movie you want in the search box and your set!
+                                Movie database used to search up movies and see reviews and cast, as well as synopsis!
                             </Card.Text>
                             <div className="projectbuttons">
-                            <Button href="https://tedmjablonowski.github.io/Movie-Genie/">Movie Genie Page</Button><Button href="https://github.com/TheBatMatt/Movie-Genie">Movie Genie GitHub</Button>
+                                <Button href="https://tedmjablonowski.github.io/Movie-Genie/">Movie Genie Page</Button><Button href="https://github.com/TheBatMatt/Movie-Genie">Movie Genie GitHub</Button>
                             </div>
                         </Card.Body>
                     </Col>
-                    <Col>2 of 2</Col>
+
+
+                    <Col>
+                        <Card.Body>
+                            <Card.Title>Blog Site</Card.Title>
+                            <img src={blogsite} width="100%" />
+                            <Card.Text>
+                            A blog website where you login to make comments but are able to view without being logged in for any blogging needs!
+                            </Card.Text>
+                            <div className="projectbuttons">
+                                <Button href="https://safe-bastion-86864.herokuapp.com/">Blog Site</Button><Button href="https://github.com/a-wiles/Blog-Site">Blog Site GitHub</Button>
+                            </div>
+                        </Card.Body>
+                    </Col>
                 </Row>
+
                 <Row>
                     <Col>1 of 3</Col>
                     <Col>2 of 3</Col>
-                    <Col>3 of 3</Col>
+                </Row>
+
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col>2 of 3</Col>
                 </Row>
             </Container>
         </div >
