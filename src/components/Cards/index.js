2e2349c54ad59f254da2298464ebd840c527098c
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-06-30 02:29:30 -0400

    Added all six needed projects

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -7,6 +7,10 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import blogsite from '../../assets/blogsite.png';
+import employeetracker from '../../assets/employeetracker.png';
+import notetaker from '../../assets/notetaker.png';
+import bword from '../../assets/thebword.png';
+import weathertracker from '../../assets/weathertracker.png';
 
 
 export default function Cards() {
@@ -16,8 +20,34 @@ export default function Cards() {
                 <Row>
                     <Col>
                         <Card.Body>
+                            <Card.Title>The B Word</Card.Title>
+                            <img src={bword} width="100%" height="100%"/>
+                            <Card.Text>
+                                LGBTQ+ blog website to create a safe enviornment to air all opinions and find resources and other people to interact with.
+                            </Card.Text>
+                            <div className="projectbuttons">
+                                <Button href="https://limitless-journey-42984.herokuapp.com/">The B Word Page</Button><Button href="https://github.com/7krich/proj2">The B Word GitHub</Button>
+                            </div>
+                        </Card.Body>
+                    </Col>
+
+                    <Col>
+                        <Card.Body>
+                            <Card.Title>Employee Tracker</Card.Title>
+                            <img src={employeetracker} width="100%" height="100%" />
+                            <Card.Text>
+                                Backend database to help keep track of employees based upon managment, where worked and salary.
+                            </Card.Text>
+                            <div className="projectbuttons">
+                                <Button href="https://drive.google.com/file/d/1VxJJQ_cashIPUErjXjNz31_Q-rdrd9Lr/view">Employee Tracker's video</Button><Button href="https://github.com/a-wiles/Employee-Tracker">Employee Tracker GitHub</Button>
+                            </div>
+                        </Card.Body>
+                    </Col>
+
+                    <Col>
+                        <Card.Body>
                             <Card.Title>Movie Genie</Card.Title>
-                            <img src={genie} width="100%" />
+                            <img src={genie} width="100%" height="100%" />
                             <Card.Text>
                                 Movie database used to search up movies and see reviews and cast, as well as synopsis!
                             </Card.Text>
@@ -26,30 +56,48 @@ export default function Cards() {
                             </div>
                         </Card.Body>
                     </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Card.Body>
+                            <Card.Title>Weather Tracker</Card.Title>
+                            <img src={weathertracker} width="100%" height="100%" />
+                            <Card.Text>
+                                Website designed to show the next 7 days of weather for a given entered city.
+                            </Card.Text>
+                            <div className="projectbuttons">
+                                <Button href="https://a-wiles.github.io/WeatherDashboard/">Weather Tracking Page</Button><Button href="https://github.com/a-wiles/WeatherDashboard">Weather Tracking GitHub</Button>
+                            </div>
+                        </Card.Body>
+                    </Col>
 
 
                     <Col>
                         <Card.Body>
                             <Card.Title>Blog Site</Card.Title>
-                            <img src={blogsite} width="100%" />
+                            <img src={blogsite} width="100%" height="100%" />
                             <Card.Text>
-                            A blog website where you login to make comments but are able to view without being logged in for any blogging needs!
+                                A blog website where you login to make comments but are able to view without being logged in for any blogging needs!
                             </Card.Text>
                             <div className="projectbuttons">
                                 <Button href="https://safe-bastion-86864.herokuapp.com/">Blog Site</Button><Button href="https://github.com/a-wiles/Blog-Site">Blog Site GitHub</Button>
                             </div>
                         </Card.Body>
                     </Col>
-                </Row>
 
-                <Row>
-                    <Col>1 of 3</Col>
-                    <Col>2 of 3</Col>
-                </Row>
-
-                <Row>
-                    <Col>1 of 3</Col>
-                    <Col>2 of 3</Col>
+                    <Col>
+                        <Card.Body>
+                            <Card.Title>Note Tracker</Card.Title>
+                            <img src={notetaker} width="100%" height="100%" />
+                            <Card.Text>
+                                Website to assist in tracking any and all notes with the ability to edit or delete notes at will.
+                            </Card.Text>
+                            <div className="projectbuttons">
+                                <Button href="https://blooming-atoll-45008.herokuapp.com/">Note Tracker Site</Button><Button href="https://github.com/a-wiles/Note-Challenge">Note Tracker GitHub</Button>
+                            </div>
+                        </Card.Body>
+                    </Col>
                 </Row>
             </Container>
         </div >
